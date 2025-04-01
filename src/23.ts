4c316a7e88ba876b0d5a4880647ded6b1f2438fa
murguiazabdi579 <murguiazabdi579@gmail.com>
2025-04-01 15:14:23 +0000

    Add 23.ts

---
+++ src/23.ts
@@ -0,0 +1,11 @@
+using System;
+
+public class MathClass {
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+}
