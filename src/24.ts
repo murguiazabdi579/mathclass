6223d0066da84505d3871f72771ab2238c9f3c03
murguiazabdi579 <murguiazabdi579@gmail.com>
2025-04-03 13:25:34 +0000

    Add 24.ts

---
+++ src/24.ts
@@ -0,0 +1,13 @@
+import math
+
+def calculate_square_root(n):
+    """
+    Calculate the square root of a given number.
+    
+    Parameters:
+    n (float): The number to find the square root of.
+    
+    Returns:
+    float: The square root of the input number.
+    """
+    return math.sqrt(n)
